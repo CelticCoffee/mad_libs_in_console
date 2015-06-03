24a96a5a69aa8df41c5a0b15ed418e4337aac810
Wendy Goodwin <wendygoodwin@mac.com>
2015-06-02 20:25:17 -0700

    message received over and out

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert("I'm working with JavaScript");
+
+function mad_libs() {
+	document.write("There were thirteen dragons but only twelve princesses that night.  How would they deal with the imbalance?  One of the dragons suggested a game of tug-o-war but another pointed out that with such tiny arms it would be hard for any but the princess to grasp the rope.  Twenty minutes of arguments and fire breathing passed before finally a wiry woman in the back of the line suggested that they play mad libs in order to fairly eliminate one of the dragons.");
+}
+
+mad_libs();
